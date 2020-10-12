03e430d7f2b51ec23a8419cff6a4ce29038bc993
RedPlus <redplus@redplus.net>
2020-10-13 00:57:10 +0900

    Logout

---
+++ Hawaso/Controllers/LogoutController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hawaso.Controllers
+{
+    public class LogoutController : Controller
+    {
+        //public async Task<IActionResult> Index()
+        //{
+        //    // 로그아웃
+        //    await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+        //    return LocalRedirect("/");
+        //}
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            // 로그아웃
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return LocalRedirect("/");
+        }
+    }
+}
